200ac3b9ce1123bd6a632fdb0aaf9664b8b46e77
JuusoMRusanen <juuso.m.rusanen@gmail.com>
2023-02-10 20:32:44 +0200

    visual overhaul

--- src/App.js
+++ src/App.js
@@ -2,72 +2,124 @@ import Valikko from './components/Valikko';
 import Selaa from './components/Selaa';
 import Etusivu from './components/Etusivu';
 import Postimerkki from './components/Postimerkki';
-import { createTheme, CssBaseline, ThemeProvider } from '@mui/material';
+import { Box, createTheme, CssBaseline, ThemeProvider } from '@mui/material';
 import { Route, Routes } from 'react-router-dom';
 
 export default function App() {
 
   const tyylit = createTheme({
-    otsikkoEtusivu : {
-        fontSize : 40,
-        margin : 20,
-        
+    palette: {
+      primary: {
+        main: '#ff6f00',
+      },
+      secondary: {
+        main: '#0044ff',
+      },
+      neutral: {
+        main: '#64748B',
+      },
+      contrastThreshold: 4.5,
     },
-    alaotsikkoEtusivu : {
-        fontSize : 20,
-        
-    },
-    otsikkoPostimerkki : {
-      fontSize : 40,
-      margin : 20,
+    appBarOtsikko:{
       
     },
+    etusivuKontti : {
+      minHeight:"100vh",
+      textAlign:"center",
+    },
+    appBar:{
+      background:"rgba(255,255,255,0.8)",
+    },
+    etusivuTekstit: {
+      background:"rgba(255,255,255,0.8)",
+      boxShadow:"2px 2px 10px",
+    },
+    otsikkoEtusivu : {
+      fontSize : "40px",
+      margin : "20px",
+      padding:"10px",
+    },
+    alaotsikkoEtusivu : {
+      fontSize : "25px",
+      padding : "20px",
+    },
+    otsikkoPostimerkki : {
+      fontSize : "40px",
+      margin : "20px",
+      paddingTop:"20px"
+    },
     alaotsikkoPostimerkki : {
-        fontSize : 20,
+      fontSize : "20px",
     },
     nappiPostimerkki : {
-        marginTop : 20,
-        padding : 20,
-        backgroundColor : 'lightgreen'
+      marginTop : "20px",
+      marginBottom : "20px",
+      padding : "10px",
+      fontSize:"15px",
+      fontWeight:"bold",
     },
     merkkiKontti : {
-      marginTop : 20
+      marginTop : "20px"
     },
     nappiKontti : {
-        marginTop : 20,
-        marginBottom : 20,
-        textAlign : 'center'
+      display:"flex",
+      justifyContent:"center",
+      gap:"10px",
+      width:"100%",
+      marginTop : "20px",
+      marginBottom : "20px",
+      marginLeft:"20px",
     },
     seuraavaNappi : {
-        padding : 20,
-        marginLeft : 10,
-        backgroundColor : 'lightgreen'
+      padding : 20,
+      fontWeight:"bold",
     },
     edellinenNappi : {
-        padding : 20,
-        marginRight : 10,
-        backgroundColor : 'lightblue'
+      padding : 20,
+      fontWeight:"bold",
+    },
+    kortti:{
+      background:"rgba(255,255,255,0.8)",
     },
     lisatietoNappi : {
-        backgroundColor : 'lightblue'
+      fontWeight:"bold"
     },
     cardTitle : {
       height : "120px"
-    }
+    },
+    merkit : {
+      width:"100%",
+      border:"1px solid red",
+    },
+    menuItem:{
+      paddingRight:"40px",
+      fontSize:"20px"
+    },
+    lisatietoKontti:{
+      background:"rgba(255,255,255,0.8)",
+      boxShadow:"2px 2px 10px",
+    },
   });
 
   return (
-    <div className="App">
-        <ThemeProvider theme={tyylit}>
-          <CssBaseline />
-          <Valikko />
-          <Routes>
-            <Route path="/" element={<Etusivu />} />
-            <Route path="/Etusivu" element={<Etusivu />} />
-            <Route path="/Selaa" element={<Selaa />} />
-            <Route path="/Selaa/:postimerkki_id" element={<Postimerkki />} />
-          </Routes>
-        </ThemeProvider>
-    </div>
+    <Box 
+      className="App" 
+      sx={{ 
+        backgroundColor:"#fff8de", 
+        backgroundImage:"url('background.jpg')",
+        backgroundSize:"cover",
+      }} 
+      >
+      <ThemeProvider theme={tyylit}>
+        <CssBaseline />
+        <Valikko />
+        <Routes>
+          <Route path="/" element={<Etusivu />} />
+          <Route path="/Etusivu" element={<Etusivu />} />
+          <Route path="/Selaa" element={<Selaa />} />
+          <Route path="/Selaa/:postimerkki_id" element={<Postimerkki />} />
+        </Routes>
+      </ThemeProvider>
+    </Box>
   );
 }
